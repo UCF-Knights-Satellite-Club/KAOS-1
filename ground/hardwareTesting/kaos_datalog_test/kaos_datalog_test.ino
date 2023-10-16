5cd8d7eadb92838a17ad75ed40b631a8e70b074f
webbgamers <webbgamers@gmail.com>
2023-10-16 13:03:00 -0400

    arduino console plot

--- ground/hardwareTesting/kaos_datalog_test/kaos_datalog_test.ino
+++ ground/hardwareTesting/kaos_datalog_test/kaos_datalog_test.ino
@@ -14,6 +14,12 @@ Adafruit_SCD30  scd30;
 Adafruit_BMP3XX bmp;
 RTC_DS1307 rtc;
 
+bool data_ready = false;
+
+void IRAM_ATTR scd30_ready() {
+  data_ready = true;
+}
+
 void setup() {
   Serial.begin(115200);
   while (!Serial);
@@ -52,61 +58,80 @@ void setup() {
     Serial.println("No SD card attached");
     return;
   }
+
+  // Configure SCD30 RDY interrupt
+  pinMode(17, INPUT_PULLDOWN);
+  attachInterrupt(17, scd30_ready, RISING);
 }
 
 void loop() {
-  if (scd30.dataReady()) {
-    if (! scd30.read()) { Serial.println("Error reading SCD30 data"); return; }
-    if (! bmp.performReading()) { Serial.println("Error reading BMP388 data"); return; }
-    if (! rtc.isrunning()) {
-      Serial.println("RTC is NOT running, setting date/time to 1-1-23/00:00:00");
-      rtc.adjust(DateTime(2023, 1, 1, 0, 0, 0));
-    }
-
-    File file = SD.open("/datalog.csv", FILE_APPEND);
-    if(!file){
-        Serial.println("Failed to open file for appending");
-        return;
-    }
-
-    // date
-    DateTime now = rtc.now();
-    file.print(now.month());
-    file.print("/");
-    file.print(now.day());
-    file.print("/");
-    file.print(now.year());
-    file.print(",");
-    
-    // time
-    file.print(now.hour());
-    file.print(":");
-    file.print(now.minute());
-    file.print(":");
-    file.print(now.second());
-    file.print(",");
-
-    // temp
-    file.print(bmp.temperature);
-    file.print(",");
-
-    // pressure
-    file.print(bmp.pressure / 100);
-    file.print(",");
-
-    // altitude
-    file.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
-    file.print(",");
-
-    // humidity
-    file.print(scd30.relative_humidity);
-    file.print(",");
-
-    // co2
-    file.print(scd30.CO2, 3);
-    file.println();
-
-    file.close();
-    delay(100);
+  while(!data_ready) { delay(10); }
+  if (! scd30.read()) { Serial.println("Error reading SCD30 data"); return; }
+  if (! bmp.performReading()) { Serial.println("Error reading BMP388 data"); return; }
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running, setting date/time to 1-1-23/00:00:00");
+    rtc.adjust(DateTime(2023, 1, 1, 0, 0, 0));
   }
+
+  File file = SD.open("/datalog.csv", FILE_APPEND);
+  if(!file){
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+
+  // date
+  DateTime now = rtc.now();
+  file.print(now.month());
+  file.print("/");
+  file.print(now.day());
+  file.print("/");
+  file.print(now.year());
+  file.print(",");
+  
+  // time
+  file.print(now.hour());
+  file.print(":");
+  file.print(now.minute());
+  file.print(":");
+  file.print(now.second());
+  file.print(",");
+
+  // temp
+  file.print(bmp.temperature);
+  file.print(",");
+  Serial.print("Temp_(C):");
+  Serial.print(bmp.temperature);
+  Serial.print(",");
+
+  // pressure
+  file.print(bmp.pressure / 100);
+  file.print(",");
+  Serial.print("Pressure_(hPa):");
+  Serial.print(bmp.pressure / 100);
+  Serial.print(",");
+
+  // altitude
+  file.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  file.print(",");
+  Serial.print("Altitude_(m):");
+  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.print(",");
+
+  // humidity
+  file.print(scd30.relative_humidity);
+  file.print(",");
+  Serial.print("Relative_Humidity:");
+  Serial.print(scd30.relative_humidity);
+  Serial.print(",");
+
+  // co2
+  file.print(scd30.CO2, 3);
+  file.println();
+  Serial.print("CO2_(ppm):");
+  Serial.print(scd30.CO2, 3);
+  Serial.println();
+
+  file.close();
+  
+  data_ready = false;
 }
